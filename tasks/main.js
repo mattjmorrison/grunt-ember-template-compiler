a60ab1adcbebb5dfbe2fe9a07481c0026b197c99
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-08-03 22:58:59 -0500

    init

---
+++ tasks/main.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+    var glob = require('glob');
+    var fs = require('fs');
+    var mkdirp = require('mkdirp');
+    var compiler = require('ember-template-compiler');
+    var dirname = require('path').dirname;
+
+    grunt.task.registerTask('emberhandlebars', 'Compile Ember Templates', function(){
+        var files = grunt.config('emberhandlebars.compile.files');
+        var dest = grunt.config('emberhandlebars.compile.dest');
+        var formatName = grunt.config('emberhandlebars.compile.options.templateName');
+
+        var output = "";
+        files.forEach(function(pattern){
+            glob.sync(pattern).forEach(function(){
+                var contents = fs.readFileSync(pathMatch).toString();
+                var compiled = compiler.precompile(input).toString();
+                output += "Ember.TEMPLATES['" + formatName(patchMatch) + "'] = Ember.Handlebars.template(" + compiled + ");";
+            });
+        });
+
+        mkdirp(dirname(dest));
+        fs.writeFileSync(dest, output);
+    });
+};
